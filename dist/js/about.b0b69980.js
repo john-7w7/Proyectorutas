bcb532a48aee019d90cddcfb145909a02d801114
john-7w7 <88951894+john-7w7@users.noreply.github.com>
2021-09-25 00:24:32 -0500

    Add files via upload

---
+++ dist/js/about.b0b69980.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,o){"use strict";o.r(e);var n=o("7a23"),c={class:"about"};function u(t,e,o,u,a,r){var b=Object(n["u"])("titulo");return Object(n["p"])(),Object(n["d"])("div",c,[Object(n["g"])(b,{texto:"Articulo"})])}var a=o("e78d"),r={components:{titulo:a["a"]}},b=o("6b0d"),i=o.n(b);e["default"]=i()(r,[["render",u]])}}]);
+//# sourceMappingURL=about.b0b69980.js.map
